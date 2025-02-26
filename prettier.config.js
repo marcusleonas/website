469a0d71171a6b05a2032c760f7dd6febf5e7dbc
flvffywvffy <marcus@marcusleonasharvey.com>
2025-02-26 19:10:01 +0000

    fix formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,13 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-    plugins: ["prettier-plugin-tailwindcss"],
-    tailwindStylesheet: "./src/styles/global.css",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tailwindStylesheet: "./src/styles/global.css",
 };
--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,20 @@
 export const SITE_TITLE = "Marcus Leonas Harvey";
+
+export const SITE_MENU: { label: string; href: string }[] = [
+  {
+    label: "/home",
+    href: "/",
+  },
+  {
+    label: "/about",
+    href: "/about",
+  },
+  {
+    label: "/shows",
+    href: "/shows",
+  },
+  {
+    label: "/contact",
+    href: "/contact",
+  },
+];
