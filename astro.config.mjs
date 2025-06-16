ca55e013cb5de00b7ece9edbb98b312193b1857f
flvffywvffy <me@flvffy.top>
2025-06-16 14:33:49 +0100

    add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://marcusleonasharvey.co.uk",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   prefetch: true,
 
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
